20b202802fd5d87519641bb5691495b3a57d734b
YERAN0105 <150114281+YERAN0105@users.noreply.github.com>
2024-03-14 22:11:30 +0530

    updated the flask part

---
+++ DSGP/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def recommend_laptop():
+    if request.method == 'POST':
+        price = request.form.get("priceRange")
+        ram = request.form.get("ram")
+        gpu = request.form.get("gpu")
+
+        jnj(price, ram, gpu)
+        return render_template('recommendationPage1.html', price=price)
+
+    # data = request.json
+    # price = data['price']
+    # ram = data['ram']
+    # gpu = data['gpu']
+    #
+    # # Now you have the user inputs, you can process them as needed
+    # print("Price:", price)
+    # print("RAM:", ram)
+    # print("GPU:", gpu)
+
+    # return jsonify({'success': True})
+    return render_template('recommendationPage2.html')
+
+
+@app.route('/sample', methods=['GET', 'POST'])
+def sample_fun():
+    if request.method == 'POST':
+        print("hello world")
+
+    return render_template('recommendationPage1.html')
+
+
+def jnj(price, ram, gpu):
+    print(price)
+    print(ram)
+    print(gpu)
+
+
+if __name__ == '__main__':
+    app.run()
