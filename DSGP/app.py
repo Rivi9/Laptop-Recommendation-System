9caf5bc8438152cbff570f366e12cac969020fd7
YERAN0105 <150114281+YERAN0105@users.noreply.github.com>
2024-03-15 21:57:43 +0530

    connected advance recommendation webpage with the flask

--- DSGP/app.py
+++ DSGP/app.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template, request, jsonify
+import pandas as pd
 
 app = Flask(__name__)
+# Load laptop data from CSV
+laptops_df = pd.read_csv("laptops_cleaned.csv")
 
 
 @app.route('/', methods=['GET', 'POST'])
-def recommend_laptop():
+def recommend():
     if request.method == 'POST':
         price = request.form.get("priceRange")
         ram = request.form.get("ram")
         gpu = request.form.get("gpu")
-
-        jnj(price, ram, gpu)
+        advance_rec_dropdown(price, ram, gpu)
         return render_template('recommendationPage1.html', price=price)
 
     # data = request.json
@@ -35,10 +37,30 @@ def sample_fun():
     return render_template('recommendationPage1.html')
 
 
-def jnj(price, ram, gpu):
-    print(price)
-    print(ram)
-    print(gpu)
+def advance_rec_dropdown(price, ram, gpu):
+    recommendation = recommend_laptop(price, ram, gpu)
+    print(recommendation)
+
+def recommend_laptop(price, ram, gpu):
+    price = float(price)
+    ram = int(ram)
+    """Recommend a laptop based on price, RAM, and GPU."""
+    # Filter laptops based on user input
+    filtered_laptops = laptops_df
+    if price:
+        filtered_laptops = filtered_laptops[filtered_laptops['Price_euros'] <= price]
+    if ram:
+        filtered_laptops = filtered_laptops[filtered_laptops['Ram'] >= ram]
+    if gpu:
+        filtered_laptops = filtered_laptops[filtered_laptops['Gpu'] == gpu]
+
+    print(len(filtered_laptops))
+    # Sort laptops by price and return the top recommendation
+    if not filtered_laptops.empty:
+        return filtered_laptops.sort_values(by='Price_euros').iloc[0]
+        #return filtered_laptops
+    else:
+        return "No laptops match the specified criteria."
 
 
 if __name__ == '__main__':
